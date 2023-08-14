cc5ab8b91012d1845175f76a0124af1552e07bc4
Mohamed hassan <67841458+MohamedHassan1311@users.noreply.github.com>
2023-08-15 00:24:02 +0300

    updates

--- lib/features/home/widgets/session_card.dart
+++ lib/features/home/widgets/session_card.dart
@@ -39,27 +39,11 @@ class HomeSessionsCard extends StatelessWidget {
             ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 12.h),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text("${product.service}",
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: AppTextStyles.semiBold
-                          .copyWith(fontSize: 14, color: Styles.PRIMARY_COLOR)),
-                  SizedBox(
-                    height: 12.h,
-                  ),
-                  Text("${product.service}",
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: AppTextStyles.regular.copyWith(
-                          height: 1,
-                          fontSize: 14,
-                          color: Styles.DETAILS_COLOR)),
-                ],
-              ),
+              child: Text("${product.service}",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: AppTextStyles.semiBold
+                      .copyWith(fontSize: 14, color: Styles.PRIMARY_COLOR)),
             ),
           ],
         ),
